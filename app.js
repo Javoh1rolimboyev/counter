d4019da817e752caf2d0b0e287c9736bab612724
Javoh1rolimboyev <javohirolimov306@gmail.com>
2026-04-08 16:30:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -37,3 +37,40 @@ window.onload = function(){
     }
       updateCount();
 };
+ const plus = document.getElementById("plus");
+
+  plus.addEventListener("mouseover", function() {
+    plus.style.backgroundColor = "red";
+  });
+
+  plus.addEventListener("mouseout", function() {
+    plus.style.backgroundColor = "white";
+  });
+
+  const minus = document.getElementById("minus");
+
+  minus.addEventListener("mouseover", function() {
+    minus.style.backgroundColor = "red";
+  });
+
+  minus.addEventListener("mouseout", function() {
+    minus.style.backgroundColor = "white";
+  });
+  const reset = document.getElementById("reset");
+
+  reset.addEventListener("mouseover", function() {
+    reset.style.backgroundColor = "green";
+  });
+
+  reset.addEventListener("mouseout", function() {
+    reset.style.backgroundColor = "red";
+  });
+  const countainer = document.getElementById("countainer");
+
+  countainer.addEventListener("mouseover", function() {
+    countainer.style.backgroundColor = "rgb(46, 47, 48)";
+  });
+
+  countainer.addEventListener("mouseout", function() {
+    countainer.style.backgroundColor = "rgb(75, 77, 70)";
+  });
